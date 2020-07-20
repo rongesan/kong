db2157e173a34eb09389d368bef623a32a86f3b2
rongesan <1679074+rongesan@users.noreply.github.com>
2020-07-20 22:13:55 +0900

    fix konga

---
+++ konga/user_seed.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        "username": "admin",
+        "email": "admin@test.com",
+        "firstName": "Admin",
+        "lastName": "admin",
+        "node_id": "http://kong:8001",
+        "admin": true,
+        "active" : true,
+        "password": "hogehoge"
+    }
+]
